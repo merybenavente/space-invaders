b72570dac156602411933698f8eda6fdd69408a9
María Benavente <3081639+merybenavente@users.noreply.github.com>
2025-11-19 19:43:35 -0700

    add functionality

---
+++ enemy.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+class EnemyState(Enum):
+  enemy_0 = """
+ _____
+ |o o|
+-| - |-
+  ┘ └
+    """
+  enemy_1 = """
+  _____
+  |o o|
+\\| O |/
+   ┘ └
+  """,
+  enemy_2 = """
+ _____
+ |* *|
+/| ~ |\\
+  ┘ └
+  """,
+  enemy_3 = """
+ _____
+ |> <|
+┌| _ |┐
+  ┘ └
+  """
+
+class Enemy:
+  def __init__(self, y: int, x: int, lifes = 4):
+    self.x = x
+    self.y = y
+    self.direction = -1
+    self.lifes = lifes
+
+ENEMY_LIFE_MAPPING = {
+  4: EnemyState.enemy_0,
+  3: EnemyState.enemy_1,
+  2: EnemyState.enemy_2,
+  1: EnemyState.enemy_3,
+}
--- space_invaders.py
+++ space_invaders.py
@@ -1,106 +1,125 @@
 import curses
 import time
+import random
 
-enemy_0 = """
- _____
- |o o|
--| - |-
- =====
-  / \
-"""
-enemy_1 = """
- _____
- |o o|
-\| O |/
- =====
-  / \
-"""
-enemy_2 = """
- _____
- |* *|
-/| ~ |\
- =====
-  / \
-"""
-enemy_3 = """
- _____
- |> <|
-/| _ |\
- =====
-  / \
-"""
-enemy_status = [enemy_0, enemy_1, enemy_2, enemy_3]
+from enemy import Enemy, ENEMY_LIFE_MAPPING
 
 
-def pause_game(stdscr):
-    # TODO
-    # until r for resume
-    stdscr.addstr("[paused]")
+class Board:
+  TITLE = "SPACE INVADERS"
+  PLATFORM = "=====^====="
 
-def shoot(stdscr, x: int, y: int):
-    # TODO
-    stdscr.addstr("[pium pium]")
+  def __init__(self, stdscr):
+    h, w = stdscr.getmaxyx()
+    self.win_h, self.win_w = h // 2, w // 2
+    start_y = (h - self.win_h) // 2
+    start_x = (w - self.win_w) // 2
 
-def create_enemy(stdscr):
-    # TODO
-    stdscr.addstr(enemy_status[0])
+    self.canvas = curses.newwin(
+      self.win_h, self.win_w,
+      start_y, start_x
+    )
 
-def move_enemies(stdscr, x: int, y: int):
-    # TODO
-    pass
+    self.canvas.keypad(True)
+    self.canvas.nodelay(True)
+    self.canvas.keypad(True)
+
+    self.state = {
+      "title": {"y": 2, "x": (self.win_w - len(self.TITLE)) // 2, "str": self.TITLE},
+      "platform": {
+        'y': self.win_h - 2,
+        'x': max(
+          1, min(
+            (self.win_w - len(self.PLATFORM)) // 2, self.win_w - 1 - len(self.PLATFORM)
+          )
+        ),
+        "str": self.PLATFORM
+      }
+    }
+    self.enemies: list[Enemy] = []
+    [self.create_enemy() for i in range(random.randint(1, 4))]
+
+    self.reprint_board()
+
+  def reprint_board(self):
+    self.canvas.erase()
+
+    for e in self.enemies:
+      sprite = ENEMY_LIFE_MAPPING[e.lifes].value.splitlines()
+      for i, line in enumerate(sprite):
+        self.canvas.addstr(e.y + i, e.x, line)
+
+    for item in self.state.values():
+      self.canvas.addstr(item['y'], item['x'], item['str'])
+
+    self.canvas.box()
+    self.canvas.refresh()
+    self.move_enemies()
+
+
+  def shoot(self):
+    plat = self.state['platform']
+    for pium_y in range(plat['y'] - 1, 0, -1):
+      self.canvas.addstr(pium_y, plat['x'] + len(self.PLATFORM) // 2, '*')
+      self.canvas.refresh()
+      time.sleep(0.05)
+      self.canvas.clear()
+      self.reprint_board()
+
+  def create_enemy(self):
+    self.enemies.append(
+      Enemy(
+        y=random.randint(4, self.win_h-20),
+        x=random.randint(4, self.win_w-6),
+        lifes=4
+      )
+    )
+
+  def move_enemies(self):
+    new_enemies_position = []
+    for e in self.enemies:
+      e.x = max(1, min(e.x + (1 * e.direction), self.win_w - 2))
+      new_enemies_position.append(e)
+      if random.random() < 0.2:
+        e.direction *= -1
+    self.enemies = new_enemies_position
+
+def pause_game(board):
+  board.canvas.addstr(board.win_h // 2, (board.win_w - len('GAME PAUSE, PRESS "R" TO RESUME')) // 2, 'GAME PAUSE, PRESS "R" TO RESUME')
+  board.canvas.refresh()
+  while True:
+    char = board.canvas.getch()
+    if char == ord('r'):
+      return
+
 
 def main(stdscr):
-    curses.curs_set(0)
-    curses.noecho()
-    curses.cbreak()
-    stdscr.nodelay(True)
-    stdscr.keypad(True)
-    stdscr.clear()
+  curses.curs_set(0)
+  curses.noecho()
+  curses.cbreak()
+  board = Board(stdscr)
 
-    platform = "=====^====="
-    key = ''
-    y, x = 10, 10
+  key = ''
+  while True:
+    key = board.canvas.getch()
 
-    h, w = stdscr.getmaxyx()
-    win_h, win_w = h - 4, w - 4
-    win = curses.newwin(win_h, win_w, 2, 2)
-    win.box()
-    # TODO: fix title
-    title_x = (w - len(" pium piuum ")) // 2
-    win.addstr(2, title_x, " pium piuum ")
+    if key == curses.KEY_LEFT:
+      board.state['platform']['x'] -= 1
+    if key == curses.KEY_RIGHT:
+      board.state['platform']['x'] += 1
 
-    plat_y = win_h - 2
-    plat_x = max(1, min((win_w - len(platform)) // 2, win_w - 1 - len(platform)))
-    win.refresh()
+    board.reprint_board()
 
-    while True:
-        key = stdscr.getch()
-        win.addstr(plat_y, plat_x, platform)
-        stdscr.refresh()
+    match key:
+      case curses.KEY_UP:
+        board.shoot()
+      case _ if key == ord('p'):
+        pause_game(board)
+      case _ if key == ord('e'):
+        board.create_enemy()
+      case _ if key == ord('q'):
+        return
 
-        if key == curses.KEY_LEFT:  plat_x -= 1
-        if key == curses.KEY_RIGHT: plat_x += 1
-
-        plat_x = max(1, min(plat_x, win_w - 1 - len(platform)))
-
-        win.erase()
-        win.box()
-
-        try:
-            win.addstr(plat_y, plat_x, platform)
-        except curses.error:
-            pass
-        win.refresh()
-        stdscr.refresh()
-
-        match key:
-            case curses.KEY_UP:
-                shoot(stdscr, x, y)
-            case _ if key == ord('p'):
-                pause_game(stdscr)
-            case _ if key == ord('q'):
-                return
-
-        time.sleep(0.05)
+    time.sleep(0.05)
 
 curses.wrapper(main)
